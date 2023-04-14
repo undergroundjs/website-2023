84a1c02c00ad2de0ef6249c8ce04c5882041fa60
Seth <11750061+itzsaga@users.noreply.github.com>
2023-04-14 15:55:52 -0500

    more tags, add hero image

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -34,6 +34,13 @@ export const SEO: React.FC<SEOProps> = ({
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image} />
+      {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:url" content={seo.url} />
